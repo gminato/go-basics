1a30a4a7d2a0ec69479b7ee8ba1f0d335c1d837a
Harshit <harshit.3851@gmail.com>
2024-05-02 19:58:45 +0530

    structs

--- stucts/structs_test.go
+++ stucts/structs_test.go
@@ -2,22 +2,29 @@ package stucts
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectange := Rectangle{10.0, 10.0}
-	got := Perimeter(rectange)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
-	rectange := Rectangle{12.0, 6.0}
-	got := Area(rectange)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+
 	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectange := Rectangle{12.0, 6.0}
+		want := 72.0
+		checkArea(t, &rectange, want)
+
+	})
+
+	t.Run("Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+		checkArea(t, &circle, want)
+	})
+
 }
--- stucts/utils.go
+++ stucts/utils.go
@@ -1,14 +1,28 @@
 package stucts
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectange Rectangle) float64 {
-	return 2 * (rectange.Width + rectange.Height)
+type Shape interface {
+	Area() float64
 }
 
-func Area(rectange Rectangle) float64 {
-	return rectange.Height * rectange.Width
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func Perimeter(rectange Rectangle) float64 {
+	return 2 * (rectange.Width + rectange.Height)
 }
